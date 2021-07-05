844618bb53877753de808698211c07f8ecbc530a
Gtail <gianwagen2020@gmail.com>
2021-07-05 20:57:15 -0300

    World Champion

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Red Bull wins");
+	alert("Verstappen did it again");
 }
 

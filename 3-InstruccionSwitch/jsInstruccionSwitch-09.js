eeeca20718c7f4cab07945a86e337ba76c6fe533
Gtail <gianwagen2020@gmail.com>
2021-07-14 12:32:20 -0300

    class 4

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,39 +1,47 @@
 function mostrar() {
   let estacion;
   let destino;
+  const precio = 15000;
   let preciofinal;
   let aumento = 0;
   let descuento = 0;
-  const precio = 15000;
 
   estacion = document.getElementById("txtIdEstacion").value;
   destino = document.getElementById("txtIdDestino").value;
 
   switch (estacion) {
     case "Invierno":
+      if (destino == "Bariloche") {
+        preciofinal = precio * 1.2;
+      } else if (destino == "Mar del Plata") {
+        preciofinal = precio * 0.9;
+      } else {
+        preciofinal = precio * 0.8;
+      }
       break;
     case "Verano":
+      if (destino == "Bariloche") {
+        preciofinal= precio * 0.8;
+      } else if (destino == "Mar del Plata") {
+        preciofinal= precio * 1.2;
+      } else {
+        preciofinal = precio * 1.1;
+      }
       break;
     case "Otoño":
     case "Primavera":
+      if (destino == "Cordoba") {
+        preciofinal = precio;
+      } else { preciofinal = precio * 1.1;}
+      }
       break;
+      alert("El precio final es " + preciofinal);
   }
-  switch (destino){
-	  case ""
-  }
-} /*una agencia de viajes debe sacar las tarifas de los viajes , se cobra $15.000 por cada estadia como base, se pide el ingreso de una estacion del año y localidad para vacacionar para poder calcular el precio final
-
-en Invierno: bariloche tiene un aumento del 20% cataratas y Cordoba tiene un descuento del 10% Mar del plata tiene un descuento del 20%
-
-en Verano: bariloche tiene un descuento del 20% cataratas y Cordoba tiene un aumento del 10% Mar del plata tiene un aumento del 20%
-
-en Otoño y Primavera: bariloche tiene un aumento del 10% cataratas tiene un aumento del 10% Mar del plata tiene un aumento del 10% y Cordoba tiene el precio sin descuen
-
-
-
-
-
-
+ /* calcular el valor de preciofinal en una sola variable
+  colocamos aumento o descuento segun cada caso de estacion y destino 
+  y luego hacemos el calculo final por UNICA VEZ despues del alert
+  preciofinal = precio + aumento - descuento;
+}
 
 
 */

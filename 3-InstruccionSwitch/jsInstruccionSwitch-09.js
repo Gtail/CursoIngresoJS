4c81500f39b786d92700264a8aa3c4f65f2e80b8
Gtail <gianwagen2020@gmail.com>
2021-07-15 14:43:04 -0300

    Class 5

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -2,46 +2,36 @@ function mostrar() {
   let estacion;
   let destino;
   const precio = 15000;
-  let preciofinal;
-  let aumento = 0;
-  let descuento = 0;
+  let precioFinal;
 
   estacion = document.getElementById("txtIdEstacion").value;
   destino = document.getElementById("txtIdDestino").value;
 
-  switch (estacion) {
+   switch (estacion) {
     case "Invierno":
       if (destino == "Bariloche") {
-        preciofinal = precio * 1.2;
+        precioFinal = precio * 1.2;
       } else if (destino == "Mar del Plata") {
-        preciofinal = precio * 0.9;
+        precioFinal = precio * 0.9;
       } else {
-        preciofinal = precio * 0.8;
+        precioFinal = precio * 0.8;
       }
       break;
     case "Verano":
       if (destino == "Bariloche") {
-        preciofinal= precio * 0.8;
+        precioFinal= precio * 0.8;
       } else if (destino == "Mar del Plata") {
-        preciofinal= precio * 1.2;
+        precioFinal= precio * 1.2;
       } else {
-        preciofinal = precio * 1.1;
+        precioFinal = precio * 1.1;
       }
       break;
     case "Otoño":
     case "Primavera":
       if (destino == "Cordoba") {
-        preciofinal = precio;
+        precioFinal = precio;
       } else { preciofinal = precio * 1.1;}
       }
       break;
-      alert("El precio final es " + preciofinal);
-  }
- /* calcular el valor de preciofinal en una sola variable
-  colocamos aumento o descuento segun cada caso de estacion y destino 
-  y luego hacemos el calculo final por UNICA VEZ despues del alert
-  preciofinal = precio + aumento - descuento;
-}
-
-
-*/
+      alert("El precio final es " + precioFinal);
+  } 
--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -1,13 +1,21 @@
-function mostrar()
-{
-	var contador;
-	var acumulador;
-	var numeroIngresado;
+function mostrar() {
+  let numero1;
+  let numero2;
+  let numero3;
+  let numero4;
+  let numero5;
+  let suma;
+  let promedio;
 
-	contador=0;
-	acumulador=0;
-	
-	
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/5;
-}//FIN DE LA FUNCIÓN
+  numero1 = parseInt(prompt("Ingrese un numero"));
+  numero2 = parseInt(prompt("Ingrese un numero"));
+  numero3 = parseInt(prompt("Ingrese un numero"));
+  numero4 = parseInt(prompt("Ingrese un numero"));
+  numero5 = parseInt(prompt("Ingrese un numero"));
+
+  suma = numero1 + numero2 + numero3 + numero4 + numero5;
+  promedio = suma / 5;
+
+  document.getElementById("txtIdSuma").value = suma;
+  document.getElementById("txtIdPromedio").value = promedio;
+} //FIN DE LA FUNCIÓN

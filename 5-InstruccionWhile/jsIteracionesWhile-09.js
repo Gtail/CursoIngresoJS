5191abebf0f434062d9b171f04b6d5b7b9bced32
Gtail <gianwagen2020@gmail.com>
2021-07-22 12:40:57 -0300

    While Done

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -3,19 +3,38 @@ Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
 function mostrar()
 {	// declarar variables
-	var banderaDelPrimero;
-	var numeroIngresado;
-	var numeroMaximo;
-	var numeroMinimo;
-	var respuesta;
-	//iniciar variables
-	banderaDelPrimero="es el primero";
-	respuesta='si';
-	while(respuesta=="si")
-	{
-		
-		respuesta=prompt("desea continuar?");
-	}
-	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimmo.value=numeroMinimo;
-}//FIN DE LA FUNCIÓN
+	let numero;
+	let respuesta;
+	let maximo;
+	let minimo;
+	let flag = 0;
+   
+	do{
+		numero = parseInt(prompt("Ingrese un numero: "));
+
+		while (isNaN(numero)) {
+			numero = parseInt(prompt("Eso no es un numero, intente nuevamente: "));
+		}
+		if(flag == 0){
+			maximo = numero;
+			minimo = numero;
+		 flag = 1;
+		} else if(numero > maximo){
+			maximo = numero;
+		} else if (numero < minimo) {
+			minimo = numero;
+		}
+
+
+   
+   
+   
+		respuesta = prompt("Quiere ingresar otro numero? ").toLowerCase();
+   
+	   
+	} while(respuesta == "si");
+
+	document.getElementById("txtIdMaximo").value; = maximo
+	document.getElementById("txtIdMinimo").value; = minimo
+
+}
--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -10,20 +10,80 @@ hasta que el usuario quiera, mostrar:
 7-Promedio de positivos.
 8-Promedios de negativos.
 9-Diferencia entre positivos y negativos, (positvos-negativos). */
-function mostrar()
-{
-	//declarar contadores y variables 
-	var respuesta;
-	var numeroIngresado;
-	var sumaNegativos=0;
+function mostrar() {
+  let numero;
+  let acumuladorNeg = 0;
+  let acumuladorPos = 0;
+  let contadorPos = 0;
+  let contadorNeg = 0;
+  let contadorCeros;
+  let contadorPar = 0;
+  let contadorImp = 0;
+  let promedio = 0;
+  let promedioNeg = 0;
+  let respuesta;
+  let diferencia = 0;
+  let flagPos = 0;
+  let flagNeg = 0;
 
-	respuesta="si";
+  do {
+    numero = parseInt(prompt("Ingrese un numero"));
+    while (isNaN(numero)) {
+      numero = parseInt(prompt("Numero invalido, Intente nuevamente "));
+    }
+    respuesta = prompt("Quiere ingresar otro numero? si/no").toLowerCase();
+  } while (respuesta == "si");
 
-	while(respuesta=="si")
-	{
-		
-		respuesta=prompt("desea continuar?");
-	}//fin del while
+  if (numero > 0) {
+    acumuladorPos += numero;
+  } else if (numero < 0) {
+    acumuladorNeg += numero;
+  } else {
+    contadorCeros++;
+  }
+  if (numero % 2 == 0) {
+    contadorPar++;
+  } else {
+    contadorImp++;
+  }
+  if (flagPos == 1) {
+    promedio = acumuladorPos / contadorPos;
+  }
+  if (flagNeg == 1) {
+    promedioNeg = acumuladorNeg / contadorNeg;
+  }
+  diferencia = acumuladorPos - acumuladorNeg;
 
-	document.write("la suma de negativos es :"+sumaNegativos);
-}//FIN DE LA FUNCIÓN
+  console.log("1- Suma de los negativos: " + acumuladorNeg);
+  console.log("2- Suma de los positivos: " + acumuladorPos);
+  console.log("3- Cantidad de positivos: " + contadorPos);
+  console.log("4- Cantidad de Negativos: " + contadorNeg);
+  console.log("5- Cantidad de Ceros: " + contadorCeros);
+  console.log("6- Cantidad de Numeros Pares: " + contadorPar);
+  console.log("7- Cantidad de Numeros Impares: " + contadorImp);
+  console.log("8- Promedio positivos: " + promedio);
+  console.log("9- Promedio negativos: " + promedioNeg);
+  console.log("10- Diferencia entre positivos y negativos: " + diferencia);
+
+}
+//estrategia de resolucion
+// 1- declarar variables
+// numero/ acumulador negativos / acumulador positivos/ contador positivos
+// contador negativos/ contador de ceros/ contador de pares/ promedio positivos / promedio/ promedio negativos
+// respuesta/ diferencia
+
+// genero un bucle del tipo mientras el usuario quiera (do-while)
+// cosas que se repiten (va dentro del bucle)
+// 2.1 pido el numero
+// 2.2 valido que sea un numero
+//2.3 identifico el signo del numero (positivo/negativo/cero con if else if else)
+// 2.3.1 positivo → acumulo positivos y lo cuento
+// 3.3.2 negativo → acumulo negativos y lo cuento
+// 3.3.3 cero → contar los numeros
+// 3.4 indicar si es par
+// 3.4.1 si es par → lo cuento
+// 4. calculo promedio de los positivos
+// 4.1 calculo promedio de negativos
+// 4.1 calculo promedio de negativos
+// 4.1 calculo promedio de negativos
+// 4.2 calculo la diferencia entre cantidad de positivos y cantidad de negativos
